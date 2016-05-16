ea5320bacf099c8c137541bb1600f61f2d4053c8
surdaft <jacktstupple@gmail.com>
2016-05-16 18:39:23 -0400

    Moved Debug class to surdaft\anook root

---
+++ src/Debug.php
@@ -0,0 +1,14 @@
+<?php namespace surdaft\anook;
+
+class Debug
+{
+    public static function printExit(...$data)
+    {
+         echo '<pre>';
+         foreach ($data as $data_item) {
+            echo print_r($data_item, true) . "\n";
+         }
+         echo '</pre>';
+         exit;
+    }
+}
