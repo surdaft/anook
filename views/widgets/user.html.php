9c91d703bc62efb98e365d1603dbe60902dfd55f
surdaft <jacktstupple@gmail.com>
2016-05-18 03:59:33 -0400

    View changes, prettier but temporary

--- views/widgets/user.html.php
+++ views/widgets/user.html.php
@@ -1,7 +1,16 @@
 <div class="anook-widget user">
     <div class="top-logo">
-        <?php print_r($anook_user); ?>
+        Anook Profile
+        <img src="<?= plugins_url('anook/images/logo/light-icon.png') ?>">
+    </div>
+    <div class="profile">
+        <div class="profile-picture">
+            <img src="<?= $anook_user->picture ?>" />
+        </div>
+        <div class="profile-information">
+            <span class="username"><?= $anook_user->username ?></span>
+            <span class="age-location"><?= $anook_user->age ?>, <?= $anook_user->country ?></span>
+        </div>
+        <a class="button follow-button" href="https://www.anook.com/<?= $anook_user->username ?>?rel=surdaft/anook">Follow</a>
     </div>
 </div>
-
-<!-- Last updated: <?= date('Y-m-d H:i:s'); ?> -->
